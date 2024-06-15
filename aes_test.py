d5b7cc4ec8d5ac7376f6c09bbb349c8869833e14
cyberalchem1st <cyberalchem1st>
2024-06-15 18:56:30 -0300

    🖌️ print formatting

--- aes_test.py
+++ aes_test.py
@@ -6,10 +6,10 @@ x = tool.encrypt('teste')
 y = tool.encrypt('teste')
 z = tool.encrypt('teste')
 
-print(x, y, z)
-print("\n")
+print(f"Encrypted: {x}, {y}, {z}\n")
+
 _x = tool.decrypt(x)
 _y = tool.decrypt(y)
 _z = tool.decrypt(z)
 
-print(_x, _y, _z)
+print(f"Decrypted: {_x}, {_y}, {_z}\n")
